b2cc52cb149b655d3a6b0b43607422a9b3263a37
Shashi Kant <shashi.coding@gmail.com>
2022-05-04 00:57:42 +0530

    Final Project Submission

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # coding-project-template
+
+This is my final project code submission for the course Introduction to Web Development with HTML, CSS, JavaScript offered by IBM at Coursera platform .
+
+4 May 2022
--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+
+function computeInterest(){
+
+    var principalValue = parseFloat(document.getElementById("principal").value);
+    
+    var interestRate = parseFloat(document.getElementById("rate").value);
+
+    var years = parseInt(document.getElementById("years").value);
+    
+    var amount = principalValue * years * interestRate / 100 ;
+    var finalYear = 2020 + years;
+
+   
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principalValue +
+    "</mark>,<br/>" + "at an interest rate of <mark>" + interestRate + "</mark>,<br/>" +
+    "You will receive an amount of <mark>" + amount + "</mark>,<br/>" + 
+    "in the year <mark>" + finalYear + "</mark>,<br/>" ;
+
     
 }
-        
+
+function ratevalue(){
+    document.getElementById("rangeValue").innerHTML = document.getElementById("rate").value;
+}
+
+function principleAmount(){
+    var principalValue = parseInt(document.getElementById("principal").value);
+    if(principalValue.value == ""){
+        alert("Please Enter Principle Value!");
+        principalValue.focus();
+    }
+    if(principalValue < 0){
+        alert("Please Enter a Positive Principle Value!");
+        document.getElementById("principal").focus();
+    }
+
+
+}
+
+
